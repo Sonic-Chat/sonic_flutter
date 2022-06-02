bc31ced8bde10e90a799b51634bc8643ffe85b2a
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-02 16:11:53 +0530

    Service Implementation account login. Closes #9

--- lib/services/auth.service.dart
+++ lib/services/auth.service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
+import 'package:sonic_flutter/dtos/auth/login_account/login_account.dto.dart';
 import 'package:sonic_flutter/dtos/auth/register_account/register_account.dto.dart';
 import 'package:sonic_flutter/enum/auth_error.enum.dart';
 import 'package:sonic_flutter/enum/general_error.enum.dart';
@@ -14,6 +15,8 @@ import 'package:firebase_auth/firebase_auth.dart' as FA;
 
 class AuthService {
   final String apiUrl;
+
+  final FA.FirebaseAuth _firebaseAuth = FA.FirebaseAuth.instance;
 
   AuthService({
     required this.apiUrl,
@@ -38,7 +41,7 @@ class AuthService {
       // Preparing the body for the request
       String body = json.encode(registerAccountDto.toJson());
 
-      // Fetching posts from the server.
+      // Registering account on the server.
       http.Response response = await http
           .post(
             url,
@@ -66,8 +69,78 @@ class AuthService {
       // Decoding account from JSON.
       Account account = Account.fromJson(json.decode(response.body));
 
-      // Returning posts.
+      // Returning account.
       return account;
+    } on SocketException {
+      log.wtf("Dedicated Server Offline");
+      throw GeneralException(
+        message: GeneralError.SOMETHING_WENT_WRONG,
+      );
+    } on TimeoutException {
+      log.wtf("Dedicated Server Offline");
+      throw GeneralException(
+        message: GeneralError.SOMETHING_WENT_WRONG,
+      );
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+        throw GeneralException(
+          message: GeneralError.SOMETHING_WENT_WRONG,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /*
+   * Service Implementation account login.
+   * @param registerAccountDto DTO Implementation for account login.
+   */
+  Future<void> loginAccount(
+    LoginAccountDto loginAccountDto,
+  ) async {
+    try {
+      // Preparing the URL for the server request.
+      Uri url = Uri.parse("$apiUrl/api/v1/auth/token");
+
+      // Preparing the headers for the request.
+      Map<String, String> headers = {
+        "Content-Type": "application/json",
+      };
+
+      // Preparing the body for the request
+      String body = json.encode(loginAccountDto.toJson());
+
+      // Fetching token from the server.
+      http.Response response = await http
+          .post(
+            url,
+            headers: headers,
+            body: body,
+          )
+          .timeout(const Duration(seconds: 10));
+
+      // Handling Errors.
+      if (response.statusCode >= 400 && response.statusCode < 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+        throw AuthException(
+            message: AuthError.values
+                .firstWhere((error) => error.toString() == body['message']));
+      } else if (response.statusCode >= 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+
+        log.e(body["message"]);
+
+        throw GeneralException(
+          message: GeneralError.SOMETHING_WENT_WRONG,
+        );
+      }
+
+      // Getting token from response
+      String token = json.decode(response.body)['token'];
+
+      await _firebaseAuth.signInWithCustomToken(token);
     } on SocketException {
       log.wtf("Dedicated Server Offline");
       throw GeneralException(
