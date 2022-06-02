5177ae941b18dbdeca8cd2f4b41ecc02ac6eca71
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-02 17:28:49 +0530

    Offline Model Implementation for Account. Closes #12

--- lib/models/account/account.model.dart
+++ lib/models/account/account.model.dart
@@ -1,22 +1,32 @@
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'account.model.g.dart';
 
 @JsonSerializable()
+@HiveType(typeId: 1)
 class Account {
+
+  @HiveField(0, defaultValue: "")
   @JsonKey(defaultValue: "")
   final String id;
 
+  @HiveField(1, defaultValue: "")
   @JsonKey(defaultValue: "")
   final String imageUrl;
 
+  @HiveField(2, defaultValue: "")
   @JsonKey(defaultValue: "")
   final String fullName;
 
+  @HiveField(3, defaultValue: "")
   @JsonKey(defaultValue: "")
   final String status;
 
+  @HiveField(4)
   final DateTime createdAt;
+
+  @HiveField(5)
   final DateTime updatedAt;
 
   Account({
--- lib/models/account/account.model.g.dart
+++ lib/models/account/account.model.g.dart
@@ -3,6 +3,59 @@
 part of 'account.model.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class AccountAdapter extends TypeAdapter<Account> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Account read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Account(
+      id: fields[0] == null ? '' : fields[0] as String,
+      imageUrl: fields[1] == null ? '' : fields[1] as String,
+      fullName: fields[2] == null ? '' : fields[2] as String,
+      status: fields[3] == null ? '' : fields[3] as String,
+      createdAt: fields[4] as DateTime,
+      updatedAt: fields[5] as DateTime,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Account obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.imageUrl)
+      ..writeByte(2)
+      ..write(obj.fullName)
+      ..writeByte(3)
+      ..write(obj.status)
+      ..writeByte(4)
+      ..write(obj.createdAt)
+      ..writeByte(5)
+      ..write(obj.updatedAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AccountAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
