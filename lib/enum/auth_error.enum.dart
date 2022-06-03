2a06ebb2d3759e8259855b493112823645751c9d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-03 16:59:21 +0530

    Update auth_error.enum.dart

--- lib/enum/auth_error.enum.dart
+++ lib/enum/auth_error.enum.dart
@@ -5,3 +5,32 @@ enum AuthError {
   WRONG_PASSWORD,
   UNAUTHENTICATED,
 }
+
+String authErrorStrings(AuthError error) {
+  switch (error) {
+    case AuthError.ACCOUNT_ALREADY_EXISTS_FOR_EMAIL:
+      {
+        return 'An account with that email address already exists.';
+      }
+    case AuthError.ACCOUNT_ALREADY_EXISTS_FOR_USERNAME:
+      {
+        return 'An account with that username already exists.';
+      }
+    case AuthError.ACCOUNT_DOES_NOT_EXIST:
+      {
+        return 'Account does not exist.';
+      }
+    case AuthError.WRONG_PASSWORD:
+      {
+        return 'Wrong Password Given.';
+      }
+    case AuthError.UNAUTHENTICATED:
+      {
+        return 'You are not authenticated.';
+      }
+    default:
+      {
+        return 'Something went wrong, please try again later.';
+      }
+  }
+}
