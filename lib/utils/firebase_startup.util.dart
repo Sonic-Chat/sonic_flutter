e8d7057e66cab5b22ca330028149f7e375105822
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-03 17:36:51 +0530

    Update firebase_startup.util.dart

--- lib/utils/firebase_startup.util.dart
+++ lib/utils/firebase_startup.util.dart
@@ -1,6 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:sonic_flutter/firebase_options.dart';
 import 'package:sonic_flutter/utils/logger.util.dart';
 
@@ -12,13 +10,4 @@ Future<void> firebaseStartup() async {
   );
 
   log.i('Firebase Initialized');
-
-  log.i('Connecting Firebase Emulators');
-
-  await FirebaseAuth.instance.useAuthEmulator(
-    'localhost',
-    9099,
-  );
-
-  log.i('Connected Firebase Emulators');
 }
