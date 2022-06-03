92880b8eef21087d65a5de200e72132c3f896920
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-03 16:59:23 +0530

    Update general_error.enum.dart

--- lib/enum/general_error.enum.dart
+++ lib/enum/general_error.enum.dart
@@ -2,3 +2,20 @@ enum GeneralError {
   SOMETHING_WENT_WRONG,
   OFFLINE,
 }
+
+String generalErrorStrings(GeneralError error) {
+  switch (error) {
+    case GeneralError.SOMETHING_WENT_WRONG:
+      {
+        return 'Something went wrong, please try again later.';
+      }
+    case GeneralError.OFFLINE:
+      {
+        return 'You are offline.';
+      }
+    default:
+      {
+        return 'Something went wrong, please try again later.';
+      }
+  }
+}
