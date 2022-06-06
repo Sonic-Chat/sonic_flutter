7022250ae09ec9ae86815675d3cc71d62c6bbf76
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-06 16:01:54 +0530

    UI Implementation for Update Profile. Closes #24

---
+++ lib/pages/account/account_update.page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:sonic_flutter/widgets/user_account/update_profile.widget.dart';
+
+class AccountUpdate extends StatelessWidget {
+  const AccountUpdate({Key? key}) : super(key: key);
+
+  static const route = "/account-update";
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Update Your Profile'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Text(
+                  'Account Details',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: MediaQuery.of(context).size.longestSide * 0.02,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'Email Details',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: MediaQuery.of(context).size.longestSide * 0.02,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'Password Details',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: MediaQuery.of(context).size.longestSide * 0.02,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'Delete Account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: MediaQuery.of(context).size.longestSide * 0.02,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ProfileUpdateTab(),
+            ProfileUpdateTab(),
+            ProfileUpdateTab(),
+            ProfileUpdateTab(),
+          ],
+        ),
+      ),
+    );
+  }
+}
