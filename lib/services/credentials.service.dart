2160962391c4f7cb623bd387306e6c981b6f5746
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-08 15:41:37 +0530

    Service Implementation for searching users. Closes #33

--- lib/services/credentials.service.dart
+++ lib/services/credentials.service.dart
@@ -5,17 +5,20 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart' as FA;
 import 'package:sonic_flutter/dtos/credentials/delete_credentials/delete_credentials.dto.dart';
+import 'package:sonic_flutter/dtos/credentials/search_credentials/search_credentials.dto.dart';
 import 'package:sonic_flutter/dtos/credentials/update_credentials.dto.dart';
 import 'package:sonic_flutter/enum/auth_error.enum.dart';
 import 'package:sonic_flutter/enum/general_error.enum.dart';
 import 'package:sonic_flutter/exceptions/auth.exception.dart';
 import 'package:sonic_flutter/exceptions/general.exception.dart';
 import 'package:sonic_flutter/models/account/account.model.dart';
+import 'package:sonic_flutter/models/public_credentials/public_credentials.model.dart';
 import 'package:sonic_flutter/services/auth.service.dart';
 import 'package:sonic_flutter/utils/logger.util.dart';
 
 class CredentialsService {
   final String apiUrl;
+  final String rawApiUrl;
 
   final FA.FirebaseAuth _firebaseAuth = FA.FirebaseAuth.instance;
 
@@ -23,8 +26,101 @@ class CredentialsService {
 
   CredentialsService({
     required this.apiUrl,
+    required this.rawApiUrl,
     required this.authService,
   });
+
+  /*
+   * Service Implementation for searching users.
+   * @param searchCredentialsDto DTO Implementation for searching users.
+   */
+  Future<List<PublicCredentials>> searchCredentials(
+    SearchCredentialsDto searchCredentialsDto,
+  ) async {
+    try {
+      // Get the logged in user details.
+      FA.User? firebaseUser = _firebaseAuth.currentUser;
+
+      // Check if user is not null.
+      if (firebaseUser == null) {
+        // If there is no user logged is using firebase, throw an exception.
+        throw AuthException(
+          message: AuthError.UNAUTHENTICATED,
+        );
+      }
+      // Fetch the ID token for the user.
+      String firebaseAuthToken =
+          await _firebaseAuth.currentUser!.getIdToken(true);
+
+      // Prepare URL and the auth header.
+      Uri url = Uri.parse(
+          "$apiUrl/api/v1/credentials/filter/:${searchCredentialsDto.search}");
+
+      // Preparing the headers for the request.
+      Map<String, String> headers = {
+        "Authorization": "Bearer $firebaseAuthToken",
+      };
+
+      // Search for users on the server
+      http.Response response = await http
+          .get(
+            url,
+            headers: headers,
+          )
+          .timeout(const Duration(seconds: 10));
+
+      // Handling Errors.
+      if (response.statusCode >= 400 && response.statusCode < 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+        throw AuthException(
+            message: AuthError.values.firstWhere((error) =>
+                error.toString().substring("AuthError.".length) ==
+                body['message']));
+      } else if (response.statusCode >= 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+
+        log.e(body["message"]);
+
+        throw GeneralException(
+          message: GeneralError.SOMETHING_WENT_WRONG,
+        );
+      }
+
+      // Getting list of public credentials.
+      List<dynamic> jsonResponse = json.decode(response.body);
+      List<PublicCredentials> credentials = jsonResponse
+          .map((credentialsJson) => PublicCredentials.fromJson(credentialsJson))
+          .toList();
+
+      // Return results.
+      return credentials;
+    } on SocketException {
+      log.wtf("Dedicated Server Offline");
+
+      // Throw offline error.
+      throw GeneralException(
+        message: GeneralError.OFFLINE,
+      );
+    } on TimeoutException {
+      log.wtf("Dedicated Server Offline");
+
+      // Throw offline error.
+      throw GeneralException(
+        message: GeneralError.OFFLINE,
+      );
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+
+        // Throw offline error.
+        throw GeneralException(
+          message: GeneralError.OFFLINE,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
 
   /*
    * Service Implementation for credentials update.
