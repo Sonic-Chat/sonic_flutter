0e918cf6acae335006144a04bf506e1d3a2ca19b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-11 17:22:50 +0530

    Service Implementation for creating friend requests. Closes #46  and Closes #47

--- lib/services/friend_request.service.dart
+++ lib/services/friend_request.service.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart' as FA;
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 import 'package:sonic_flutter/constants/hive.constant.dart';
+import 'package:sonic_flutter/dtos/friend_request/create_friend_request/create_friend_request.dto.dart';
 import 'package:sonic_flutter/dtos/friend_request/fetch_friend_request/fetch_friend_request.dto.dart';
 import 'package:sonic_flutter/dtos/friend_request/fetch_friend_requests/fetch_friend_requests.dto.dart';
 import 'package:sonic_flutter/enum/auth_error.enum.dart';
@@ -187,6 +188,104 @@ class FriendRequestService {
           FriendRequest.fromJson(json.decode(response.body));
 
       // Returning requests.
+      return request;
+    } on SocketException {
+      log.wtf("Dedicated Server Offline");
+
+      throw GeneralException(
+        message: GeneralError.OFFLINE,
+      );
+    } on TimeoutException {
+      log.wtf("Dedicated Server Offline");
+
+      throw GeneralException(
+        message: GeneralError.OFFLINE,
+      );
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+
+        throw GeneralException(
+          message: GeneralError.OFFLINE,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /*
+   * Service Implementation for creating friend requests.
+   * @param createFriendRequestDto DTO Implementation for creating friend requests.
+   */
+  Future<FriendRequest> createFriendRequest(
+    CreateFriendRequestDto createFriendRequestDto,
+  ) async {
+    try {
+      // Get the logged in user details.
+      FA.User? firebaseUser = _firebaseAuth.currentUser;
+
+      // Check if user is not null.
+      if (firebaseUser == null) {
+        // If there is no user logged is using firebase, throw an exception.
+        throw AuthException(
+          message: AuthError.UNAUTHENTICATED,
+        );
+      }
+      // Fetch the ID token for the user.
+      String firebaseAuthToken =
+          await _firebaseAuth.currentUser!.getIdToken(true);
+
+      // Prepare URL and the auth header.
+      Uri url = Uri.parse("$apiUrl/api/v1/friends");
+
+      // Preparing body for the request.
+      String body = json.encode(createFriendRequestDto.toJson());
+
+      // Preparing the headers for the request.
+      Map<String, String> headers = {
+        "Authorization": "Bearer $firebaseAuthToken",
+        "Content-Type": "application/json",
+      };
+
+      // Posting new request from the server
+      http.Response response = await http
+          .post(
+            url,
+            headers: headers,
+            body: body,
+          )
+          .timeout(const Duration(seconds: 10));
+
+      // Handling Errors.
+      if (response.statusCode >= 400 && response.statusCode < 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+        throw FriendRequestException(
+            message: FriendError.values.firstWhere((error) =>
+                error.toString().substring("FriendError.".length) ==
+                body['message']));
+      } else if (response.statusCode >= 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+
+        log.e(body["message"]);
+
+        throw GeneralException(
+          message: GeneralError.SOMETHING_WENT_WRONG,
+        );
+      }
+
+      // Decoding request from JSON.
+      FriendRequest request =
+          FriendRequest.fromJson(json.decode(response.body));
+
+      // Refreshing offline DB.
+      await fetchFriendRequests(
+        FetchFriendRequestsDto(
+          status: null,
+        ),
+      );
+
+      // Returning request.
       return request;
     } on SocketException {
       log.wtf("Dedicated Server Offline");
