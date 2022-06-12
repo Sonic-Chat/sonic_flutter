52f990d9178c39bd6f5b6ca223f523a9758da6af
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-12 10:43:35 +0530

    DTO Implementation for deleting friend requests. Closes #50

---
+++ lib/dtos/friend_request/delete_friend_request/delete_friend_request.dto.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'delete_friend_request.dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DeleteFriendRequestDto _$DeleteFriendRequestDtoFromJson(
+        Map<String, dynamic> json) =>
+    DeleteFriendRequestDto(
+      id: json['id'] as String,
+    );
+
+Map<String, dynamic> _$DeleteFriendRequestDtoToJson(
+        DeleteFriendRequestDto instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+    };
