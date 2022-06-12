8ad8badb90c081dfd846cf28ca11a5d0d31ed9a6
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-12 10:43:47 +0530

    Service Implementation for deleting friend requests. Closes #51

--- lib/services/friend_request.service.dart
+++ lib/services/friend_request.service.dart
@@ -7,6 +7,7 @@ import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 import 'package:sonic_flutter/constants/hive.constant.dart';
 import 'package:sonic_flutter/dtos/friend_request/create_friend_request/create_friend_request.dto.dart';
+import 'package:sonic_flutter/dtos/friend_request/delete_friend_request/delete_friend_request.dto.dart';
 import 'package:sonic_flutter/dtos/friend_request/fetch_friend_request/fetch_friend_request.dto.dart';
 import 'package:sonic_flutter/dtos/friend_request/fetch_friend_requests/fetch_friend_requests.dto.dart';
 import 'package:sonic_flutter/dtos/friend_request/update_friend_request/update_friend_request.dto.dart';
@@ -386,6 +387,97 @@ class FriendRequestService {
 
       // Returning request.
       return request;
+    } on SocketException {
+      log.wtf("Dedicated Server Offline");
+
+      throw GeneralException(
+        message: GeneralError.OFFLINE,
+      );
+    } on TimeoutException {
+      log.wtf("Dedicated Server Offline");
+
+      throw GeneralException(
+        message: GeneralError.OFFLINE,
+      );
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+
+        throw GeneralException(
+          message: GeneralError.OFFLINE,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /*
+   * Service Implementation for deleting friend requests.
+   * @param deleteFriendRequestDto DTO Implementation for deleting friend requests.
+   */
+  Future<void> deleteFriendRequest(
+    DeleteFriendRequestDto deleteFriendRequestDto,
+  ) async {
+    try {
+      // Get the logged in user details.
+      FA.User? firebaseUser = _firebaseAuth.currentUser;
+
+      // Check if user is not null.
+      if (firebaseUser == null) {
+        // If there is no user logged is using firebase, throw an exception.
+        throw AuthException(
+          message: AuthError.UNAUTHENTICATED,
+        );
+      }
+      // Fetch the ID token for the user.
+      String firebaseAuthToken =
+          await _firebaseAuth.currentUser!.getIdToken(true);
+
+      // Prepare URL and the auth header.
+      Uri url = Uri.parse("$apiUrl/api/v1/friends");
+
+      // Preparing body for the request.
+      String body = json.encode(deleteFriendRequestDto.toJson());
+
+      // Preparing the headers for the request.
+      Map<String, String> headers = {
+        "Authorization": "Bearer $firebaseAuthToken",
+        "Content-Type": "application/json",
+      };
+
+      // Deleting request from the server
+      http.Response response = await http
+          .delete(
+            url,
+            headers: headers,
+            body: body,
+          )
+          .timeout(const Duration(seconds: 10));
+
+      // Handling Errors.
+      if (response.statusCode >= 400 && response.statusCode < 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+        throw FriendRequestException(
+            message: FriendError.values.firstWhere((error) =>
+                error.toString().substring("FriendError.".length) ==
+                body['message']));
+      } else if (response.statusCode >= 500) {
+        Map<String, dynamic> body = json.decode(response.body);
+
+        log.e(body["message"]);
+
+        throw GeneralException(
+          message: GeneralError.SOMETHING_WENT_WRONG,
+        );
+      }
+
+      // Refreshing offline DB.
+      await fetchFriendRequests(
+        FetchFriendRequestsDto(
+          status: null,
+        ),
+      );
     } on SocketException {
       log.wtf("Dedicated Server Offline");
 
