61e9fb59db2ae62b1a0f801eb47974683c1a6d92
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-12 12:58:39 +0530

    Friend Status Enum Hive Setup

--- lib/enum/friend_status.enum.dart
+++ lib/enum/friend_status.enum.dart
@@ -1,5 +1,16 @@
+import 'package:hive/hive.dart';
+
+part 'friend_status.enum.g.dart';
+
+@HiveType(typeId: 4)
 enum FriendStatus {
+
+  @HiveField(0)
   REQUESTED,
+
+  @HiveField(1)
   ACCEPTED,
+
+  @HiveField(2)
   IGNORED,
 }
--- lib/utils/hive_startup.util.dart
+++ lib/utils/hive_startup.util.dart
@@ -1,6 +1,7 @@
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sonic_flutter/constants/hive.constant.dart';
+import 'package:sonic_flutter/enum/friend_status.enum.dart';
 import 'package:sonic_flutter/models/account/account.model.dart';
 import 'package:sonic_flutter/models/friend_request/friend_request.model.dart';
 import 'package:sonic_flutter/models/public_credentials/public_credentials.model.dart';
@@ -15,6 +16,7 @@ Future<void> hiveStartup() async {
   Hive.registerAdapter<Account>(AccountAdapter());
   Hive.registerAdapter<PublicCredentials>(PublicCredentialsAdapter());
   Hive.registerAdapter<FriendRequest>(FriendRequestAdapter());
+  Hive.registerAdapter<FriendStatus>(FriendStatusAdapter());
   log.i("Registered Hive Adaptors");
 
   log.i("Opening Hive Boxes");
