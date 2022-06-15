a2798b926b5d0f6b722b51e6d239529c37e5cb5e
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-15 15:29:37 +0530

    DTO Implementation for syncing messages. Closes #63

---
+++ lib/dtos/chat_message/sync_message/sync_message.dto.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sync_message.dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SyncMessageDto _$SyncMessageDtoFromJson(Map<String, dynamic> json) =>
+    SyncMessageDto(
+      authorization: json['authorization'] as String? ?? '',
+    );
+
+Map<String, dynamic> _$SyncMessageDtoToJson(SyncMessageDto instance) =>
+    <String, dynamic>{
+      'authorization': instance.authorization,
+    };
