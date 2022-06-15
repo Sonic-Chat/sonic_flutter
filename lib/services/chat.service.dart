3066127dd76097355eeeb11ddd6873f7ac941fed
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-15 15:50:48 +0530

    Service Implementation for sending messages. Closes #66

--- lib/services/chat.service.dart
+++ lib/services/chat.service.dart
@@ -2,6 +2,9 @@ import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart' as FA;
 import 'package:sonic_flutter/dtos/chat_message/connect_server/connect_server.dto.dart';
+import 'package:sonic_flutter/dtos/chat_message/send_image/send_image.dto.dart';
+import 'package:sonic_flutter/dtos/chat_message/send_message/send_message.dto.dart';
+import 'package:sonic_flutter/dtos/chat_message/send_message_image/send_message_image.dto.dart';
 import 'package:sonic_flutter/dtos/chat_message/sync_message/sync_message.dto.dart';
 import 'package:sonic_flutter/enum/auth_error.enum.dart';
 import 'package:sonic_flutter/enum/general_error.enum.dart';
@@ -90,6 +93,159 @@ class ChatService {
       Map<String, dynamic> body = {
         "event": "sync-message",
         "data": syncMessageDto.toJson(),
+      };
+
+      // Returning JSON format of the body.
+      return json.encode(body);
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+        throw GeneralException(
+          message: GeneralError.OFFLINE,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /*
+   * Service Implementation for sending text message.
+   */
+  Future<String> sendTextMessage({
+    required String message,
+    required String chatId,
+  }) async {
+    try {
+      // Get the logged in user details.
+      FA.User? firebaseUser = _firebaseAuth.currentUser;
+
+      // Check if user is not null.
+      if (firebaseUser == null) {
+        // If there is no user logged is using firebase, throw an exception.
+        throw AuthException(
+          message: AuthError.UNAUTHENTICATED,
+        );
+      }
+      // Fetch the ID token for the user.
+      String firebaseAuthToken =
+          await _firebaseAuth.currentUser!.getIdToken(true);
+
+      // Preparing DTO for the request.
+      SendMessageDto sendMessageDto = SendMessageDto(
+        authorization: firebaseAuthToken,
+        message: message,
+        chatId: chatId,
+      );
+
+      // Preparing body for the request.
+      Map<String, dynamic> body = {
+        "event": "create-message",
+        "data": sendMessageDto.toJson(),
+      };
+
+      // Returning JSON format of the body.
+      return json.encode(body);
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+        throw GeneralException(
+          message: GeneralError.OFFLINE,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /*
+   * Service Implementation for sending image.
+   */
+  Future<String> sendImage({
+    required String firebaseId,
+    required String imageUrl,
+    required String chatId,
+  }) async {
+    try {
+      // Get the logged in user details.
+      FA.User? firebaseUser = _firebaseAuth.currentUser;
+
+      // Check if user is not null.
+      if (firebaseUser == null) {
+        // If there is no user logged is using firebase, throw an exception.
+        throw AuthException(
+          message: AuthError.UNAUTHENTICATED,
+        );
+      }
+      // Fetch the ID token for the user.
+      String firebaseAuthToken =
+          await _firebaseAuth.currentUser!.getIdToken(true);
+
+      // Preparing DTO for the request.
+      SendImageDto sendImageDto = SendImageDto(
+        authorization: firebaseAuthToken,
+        firebaseId: firebaseId,
+        imageUrl: imageUrl,
+        chatId: chatId,
+      );
+
+      // Preparing body for the request.
+      Map<String, dynamic> body = {
+        "event": "create-message",
+        "data": sendImageDto.toJson(),
+      };
+
+      // Returning JSON format of the body.
+      return json.encode(body);
+    } on FA.FirebaseAuthException catch (error) {
+      if (error.code == "network-request-failed") {
+        log.wtf("Firebase Server Offline");
+        throw GeneralException(
+          message: GeneralError.OFFLINE,
+        );
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  /*
+   * Service Implementation for sending image message.
+   */
+  Future<String> sendMessageImage({
+    required String message,
+    required String firebaseId,
+    required String imageUrl,
+    required String chatId,
+  }) async {
+    try {
+      // Get the logged in user details.
+      FA.User? firebaseUser = _firebaseAuth.currentUser;
+
+      // Check if user is not null.
+      if (firebaseUser == null) {
+        // If there is no user logged is using firebase, throw an exception.
+        throw AuthException(
+          message: AuthError.UNAUTHENTICATED,
+        );
+      }
+      // Fetch the ID token for the user.
+      String firebaseAuthToken =
+          await _firebaseAuth.currentUser!.getIdToken(true);
+
+      // Preparing DTO for the request.
+      SendMessageImageDto sendMessageImageDto = SendMessageImageDto(
+        authorization: firebaseAuthToken,
+        message: message,
+        chatId: chatId,
+        firebaseId: firebaseId,
+        imageUrl: imageUrl,
+      );
+
+      // Preparing body for the request.
+      Map<String, dynamic> body = {
+        "event": "create-message",
+        "data": sendMessageImageDto.toJson(),
       };
 
       // Returning JSON format of the body.
