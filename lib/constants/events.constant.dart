91999431076c6aea7185092b66e404d40c1c1e42
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-16 19:29:51 +0530

    Update events.constant.dart

--- lib/constants/events.constant.dart
+++ lib/constants/events.constant.dart
@@ -1,1 +1,7 @@
 const CREATE_MESSAGE_EVENT = "create-message";
+const UPDATE_MESSAGE_EVENT = "update-message";
+const DELETE_MESSAGE_EVENT = "delete-message";
+const SYNC_CHAT_EVENT = "sync-chat";
+const DELIVERY_EVENT = "mark-delivered";
+const SEEN_EVENT = "mark-seen";
+const ERROR_EVENT = "error";
