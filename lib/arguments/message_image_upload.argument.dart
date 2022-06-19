38fb2c8f1cf14cceae39005bab99d070e4256f88
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2022-06-19 16:02:05 +0530

    Create message_image_upload.argument.dart

---
+++ lib/arguments/message_image_upload.argument.dart
@@ -0,0 +1,9 @@
+class MessageImageUploadArgument {
+  final String firebaseId;
+  final String imageUrl;
+
+  MessageImageUploadArgument({
+    required this.firebaseId,
+    required this.imageUrl,
+  });
+}
